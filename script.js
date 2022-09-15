3893f247c7489d584ecdda96c4f869c640d6fa96
Albert Bru <68111494+A1bert04@users.noreply.github.com>
2022-09-15 21:59:12 +0200

    3

--- script.js
+++ script.js
@@ -28,3 +28,24 @@ window.addEventListener('scroll', function () {
     }
 });
 
+// Funcion para sber si la anchura es mas de 768px
+function isLargeScreen() {
+    return window.matchMedia("(min-width: 768px)").matches;
+}
+
+// Conjunto de estilos que hay que cargar en ciertos casos
+function stylesOnLoad() {
+    // Si la anchura es menos de 768px, cambiamos algunos estilos
+    if (!isLargeScreen()) {
+        let webChapter = document.querySelector('div.web-chapter');
+        let index = document.querySelector('div.index');
+        webChapter.style.paddingLeft = '5%';
+        webChapter.style.paddingRight = '5%';
+        webChapter.style.width = '100%';
+        index.style.display = 'none';
+        // console.log('Cambiando estilos');
+    }
+
+}
+
+stylesOnLoad();
