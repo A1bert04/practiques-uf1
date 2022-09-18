d5050858a544cd10b21ba3599a8c666303e6895e
Albert Bru <68111494+A1bert04@users.noreply.github.com>
2022-09-18 11:36:36 +0200

    3

--- script.js
+++ script.js
@@ -1,55 +1,4 @@
-// Funcion para saber si un elemento esta dentro de la pantalla
-function isElementInViewport(el) {
-    rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-// Creamos un evento scroll que oculta o muestra el titulo en la nav bar
-// en funcion de si el header esta visible o no
-window.addEventListener('scroll', function () {
-    // Los elementos
-    let bigTitle = document.querySelector('header');
-    let navPractiques = document.querySelector('#NavPractiques');
-
-    if (isElementInViewport(bigTitle)) {
-        // Si esta visible el titulo grande, ocultamos el elemento
-        navPractiques.style.display = 'none';
-        // console.log('Ocultar');
-    } else {
-        // Si no esta visible el titulo grande, mostramos el elemento
-        navPractiques.style.display = 'inline-block';
-        // console.log('Mostrar');
-        // TODO Quitar los console.log
-        
-    }
-});
-
-// Creamos un evento scroll para cambiar la posicion del indice
-// de relativa a fixed en funcion de si el un elemento esta visible o no
-// window.addEventListener('scroll', function () {
-//     // Los elementos
-//     let firstTitle = document.querySelector('#first-title');
-//     let index = document.querySelector('.index');
-
-//     if (isElementInViewport(firstTitle)) {
-//         // Si esta visible el titulo grande, ponemos la pos en relative
-//         index.style.position = 'relative';
-//         // console.log('Relative');
-//     } else {
-//         // Si no esta visible el titulo grande, ponemos la pos en fixed
-//         index.style.position = 'fixed';
-//         // console.log('Fixed');
-//         // TODO Quitar los console.log
-//     }
-// });
-
-
-// Funcion para sber si la anchura es mas de 768px
+// Funcion para saber si la anchura es mas de 768px
 function isLargeScreen() {
     return window.matchMedia("(min-width: 768px)").matches;
 }
@@ -60,13 +9,50 @@ function stylesOnLoad() {
     if (!isLargeScreen()) {
         let webChapter = document.querySelector('div.web-chapter');
         let index = document.querySelector('div.index');
+        let nav = document.querySelector('nav');
         webChapter.style.paddingLeft = '5%';
         webChapter.style.paddingRight = '5%';
+        nav.style.paddingLeft = '5%';
+        nav.style.paddingRight = '5%';
         webChapter.style.width = '100%';
         index.style.display = 'none';
         // console.log('Cambiando estilos');
     }
 
+    // Hace el padding de la parte de arriba de el contenido
+    let navHeight = document.querySelector('nav').offsetHeight;
+    let content = document.querySelector('span.content');
+    content.style.paddingTop = navHeight + 'px';
+
+    
 }
 
 stylesOnLoad();
+
+// Funcion para hacer autoscroll a un elemento + 100% de la pantalla
+function scrollToElement(element) {
+    let rect = element.getBoundingClientRect();
+    let top = rect.top + window.scrollY;
+    window.scrollTo({
+        top: top,
+        behavior: 'smooth'
+    });
+}
+
+// Seleccionamos los elementos del indice y de los botones
+var indexButtons = document.querySelectorAll('.scroll-onclick');
+var targetElements = document.querySelectorAll('.target-element');
+
+// TODO Recordar quitar estos console.log
+console.log(indexButtons);
+console.log(targetElements);
+
+// Para cada boton del indice, le asignamos un evento onclick que hace scroll al target 
+// correspondiente (con mismo i en el otro array)
+for (let i = 0; i < indexButtons.length; i++) {
+    indexButtons[i].addEventListener('click', function () {
+        scrollToElement(targetElements[i]);
+    });
+}
+
+
